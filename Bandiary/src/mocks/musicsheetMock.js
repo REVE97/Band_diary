7a1f20178d667caeb1aa795a427176c4cac6ec4a
REVE97 <zxcv9675@naver.com>
2026-05-29 09:53:36 +0900

    Refactor: 테스트 파일 삭제

--- Bandiary/src/mocks/musicsheetMock.js
+++ Bandiary/src/mocks/musicsheetMock.js
@@ -1,23 +1,13 @@
-import example01 from '../assets/pdf/example_01.pdf'
-import example02 from '../assets/pdf/example_02.pdf'
+import lucky from '../assets/pdf/lucky.pdf'
 
 export const musicsheetMockList = [
   {
     id: 1,
     type: 'pdf',
-    title: '정보처리기사 이론 1',
-    description: '정보처리기사 이론 1 입니다.',
-    fileName: 'example01.pdf',
-    pdfUrl: example01,
-    tags: ['정보처리기사', '이론', 'Programming'],
-  },
-  {
-    id: 2,
-    type: 'pdf',
-    title: '정보처리기사 이론 2',
-    description: '정보처리기사 이론 2 입니다',
-    fileName: 'example02.pdf',
-    pdfUrl: example02,
-    tags: ['정보처리기사', '이론', 'CS'],
+    title: '행운을 빌어요',
+    description: '행운을 빌어요 기타 악보',
+    fileName: 'lucky.pdf',
+    pdfUrl: lucky,
+    tags: ['행운을 빌어요', '기타'],
   },
 ]
